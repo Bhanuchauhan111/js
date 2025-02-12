094051fbe6319730b1a8e96fdd5319d60947b787
Bhanuchauhan111 <61932532+Bhanuchauhan111@users.noreply.github.com>
2025-02-12 20:08:02 +0000

    variable and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 12345
+let accountEmail = "Bhanuchauhan280@gmail.com"
+var accountPassword = "bhanu"
+accountCity = "Gurgaon"
+
+// accountId = 2
+
+let accountState;
+
+/*
+prefer not to use var
+because of isuue in block scope and functional scope
+
+/*
+
+accountEmail = "p.vashisht007@gamail.com"
+accountPassword = "65747"
+accountCity = "jaipur"
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,12 @@
+let name = "bhanu"
+let age = 25
+let isLoggedIn = true
+let state = null
+
+console.log(typeof state)
+
+// number => 2 to power 53
+// "bhanu"=> string
+// true , false => bolean value
+// state => null => object
+// undefined 
