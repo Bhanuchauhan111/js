0db4c52f1f94b9a11d70e9b31f5d043be88bd2d4
Bhanuchauhan111 <61932532+Bhanuchauhan111@users.noreply.github.com>
2025-02-13 07:55:09 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,20 @@
+let score = "abc33"
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+
+// console.log(valueInNumber)
+
+
+let isLoggedIn = "bhanu"
+console.log(typeof isLoggedIn)
+
+
+let valueInLogged = Boolean(isLoggedIn)
+
+console.log(valueInLogged)
+// 1 => true
+// 0=> false
+// "" => true
