e4e102fbb0a048572b1487ca056d408a175afece
Bhanuchauhan111 <61932532+Bhanuchauhan111@users.noreply.github.com>
2025-02-15 18:21:06 +0000

    date and time

---
+++ 01_basics/08_dates_times.js
@@ -0,0 +1,24 @@
+let mydate = new Date
+// console.log(mydate);
+
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocaleString());
+
+// console.log(mydate.toLocaleDateString());
+
+// console.log(typeof mydate)
+
+
+let myCreated = new Date("01-04-2024")
+
+// console.log(myCreated.toDateString())
+
+ //console.log(myCreated.toLocaleDateString())
+
+console.log(myCreated.toLocaleString())
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp)
+
+console.log(myCreated.getTime());
