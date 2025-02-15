0b05d16dbfd13b57a6da66039f28b07026aa63e4
Bhanuchauhan111 <61932532+Bhanuchauhan111@users.noreply.github.com>
2025-02-15 17:52:19 +0000

    nums and math

---
+++ 01_basics/07_nums_maths.js
@@ -0,0 +1,28 @@
+const score = 100
+console.log(score)
+
+const anotherScore = new Number(345)
+//console.log(anotherScore);
+
+//console.log(anotherScore.toString().length)
+
+const anotherNumber = 123.333
+//console.log(anotherScore.toFixed(1))
+
+//console.log(anotherNumber.toPrecision(4))
+
+
+const OtherScore = 1000000
+//console.log(OtherScore.toLocaleString('en-In'))
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++Maths
+
+console.log(Math.abs(-4));
+
+console.log(Math.round(4.3))
+
+console.log(Math.ceil(4.7))
+console.log(Math.floor(4.3))
+
+console.log(Math.min(4,5,5,67,78,1))
+
+console.log(Math.max(2,4,5,6,7,89,7))
